06c10fbdc85dff62936c722c03c05ab2ad58fc2c
Anique <aniquedavla@gmail.com>
2017-02-25 13:22:19 -0800

    20 min prog

---
+++ src/ATMSystem.java
@@ -0,0 +1,9 @@
+/**
+ * Created by aniquedavla on 2/25/17.
+ */
+public class ATMSystem {
+    public static void main(String[] args){
+        Bank bankA = new Bank("b1A");
+        Account aniqueAcct = new Account("")
+    }
+}
